deeda0709c7a8c93093a0b017de297a6fdd27600
Kris Hayzelden <hayzelnut@sloyo.home>
2019-08-18 22:52:54 +0100

    Initial commit

---
+++ ClickerGame/ClickerGame/Form1.Designer.vb
@@ -0,0 +1,129 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.Score = New System.Windows.Forms.TextBox()
+        Me.TimeLeft = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.StartButton = New System.Windows.Forms.Button()
+        Me.Timer2 = New System.Windows.Forms.Timer(Me.components)
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.BackgroundImage = CType(resources.GetObject("PictureBox1.BackgroundImage"), System.Drawing.Image)
+        Me.PictureBox1.Location = New System.Drawing.Point(30, 82)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(25, 23)
+        Me.PictureBox1.TabIndex = 0
+        Me.PictureBox1.TabStop = False
+        Me.PictureBox1.Visible = False
+        '
+        'Score
+        '
+        Me.Score.Location = New System.Drawing.Point(672, 430)
+        Me.Score.Name = "Score"
+        Me.Score.Size = New System.Drawing.Size(100, 20)
+        Me.Score.TabIndex = 1
+        Me.Score.Text = "0"
+        Me.Score.TextAlign = System.Windows.Forms.HorizontalAlignment.Right
+        '
+        'TimeLeft
+        '
+        Me.TimeLeft.Location = New System.Drawing.Point(672, 12)
+        Me.TimeLeft.Name = "TimeLeft"
+        Me.TimeLeft.Size = New System.Drawing.Size(100, 20)
+        Me.TimeLeft.TabIndex = 2
+        Me.TimeLeft.Text = "10"
+        Me.TimeLeft.TextAlign = System.Windows.Forms.HorizontalAlignment.Right
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(583, 15)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(83, 13)
+        Me.Label1.TabIndex = 3
+        Me.Label1.Text = "Time Remaining"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(631, 433)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(35, 13)
+        Me.Label2.TabIndex = 4
+        Me.Label2.Text = "Score"
+        '
+        'Timer1
+        '
+        Me.Timer1.Interval = 1000
+        '
+        'StartButton
+        '
+        Me.StartButton.Location = New System.Drawing.Point(12, 10)
+        Me.StartButton.Name = "StartButton"
+        Me.StartButton.Size = New System.Drawing.Size(75, 23)
+        Me.StartButton.TabIndex = 5
+        Me.StartButton.Text = "Start"
+        Me.StartButton.UseVisualStyleBackColor = True
+        '
+        'Timer2
+        '
+        Me.Timer2.Enabled = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.White
+        Me.ClientSize = New System.Drawing.Size(784, 462)
+        Me.Controls.Add(Me.StartButton)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.TimeLeft)
+        Me.Controls.Add(Me.Score)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents PictureBox1 As System.Windows.Forms.PictureBox
+    Friend WithEvents Score As System.Windows.Forms.TextBox
+    Friend WithEvents TimeLeft As System.Windows.Forms.TextBox
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Timer1 As System.Windows.Forms.Timer
+    Friend WithEvents StartButton As System.Windows.Forms.Button
+    Friend WithEvents Timer2 As System.Windows.Forms.Timer
+
+End Class
---
+++ ClickerGame/ClickerGame/Form1.vb
@@ -0,0 +1,56 @@
+﻿Public Class Form1
+
+    Dim RandomNumber As Integer 'the variable for the x cordinate of picture 1
+    Dim RandomNumber2 As Integer 'the variable for the y cordiante of picture 1
+
+    Private Sub PictureBox1_Click(sender As Object, e As EventArgs) Handles PictureBox1.Click
+
+        Dim RandomClass As New Random()
+
+        RandomNumber = RandomClass.Next(750)
+        'Creates number for the x axis
+        RandomNumber2 = RandomClass.Next(450)
+        'Creates number for the y axis
+
+        PictureBox1.Location = New Point(RandomNumber, RandomNumber2) 'sends the "bug" to the new x,y cordinates        Score.Text += 1 'adds one to the score
+    End Sub
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+
+        TimeLeft.Text -= 1 'removes 1 digit from the timer count
+
+
+        If (TimeLeft.Text = 0) Then 'checks for when the time left counter hits 0
+            Timer1.Enabled = False 'stops the timer
+            PictureBox1.Visible = False 'hides the "bug"
+            MsgBox("TIME UP ~ LOL N00B U GOT " & Score.Text & " PNTS") 'prints a game over message and the score
+        End If
+
+    End Sub
+
+    Private Sub StartButton_Click(sender As Object, e As EventArgs) Handles StartButton.Click
+
+        StartButton.Visible = False 'hides the start button when the game begins
+        PictureBox1.Visible = True 'shows the "bug"
+        Timer1.Enabled = True 'starts the timer
+
+
+    End Sub
+
+    Private Sub Timer2_Tick(sender As Object, e As EventArgs) Handles Timer2.Tick
+
+        Dim Counter As Integer = 0
+        Dim RandomDirection As New Random()
+
+        If (RandomNumber > 750 Or RandomNumber2 > 450) Then
+            RandomNumber += RandomDirection.Next(20) 'generates a new random direction to go to if the x or y is too high (which would take it off screen) 
+            RandomNumber2 += RandomDirection.Next(20)
+        Else
+            RandomNumber += 10
+            RandomNumber2 += 10
+        End If
+
+        PictureBox1.Location = New Point(RandomNumber, RandomNumber2) 'slides the bug gradually to the cordinate
+
+    End Sub
+End Class
